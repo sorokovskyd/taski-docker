3fbe697a3673c778a13d56186df1344b7f4a8348
Дмитрий Сороковский <sorokovskij67@mail.ru>
2026-03-27 14:20:56 +0300

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,16 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = ['158.160.229.216', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
